55e01c623421d1c4dee1ad72d1ef8501f63ac95b
Adedayo Ayomide <adefeyisayo998@gmail.com>
2023-01-05 13:14:40 +0100

    created a Basic annotations - sum_list

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -2,11 +2,12 @@
 from typing import List
 """ Basic annotations - sum_list """
 
+
 def sum_list(input_list: List[float]) -> float:
     """a type-annotated function sum_list which takes
     a list input_list of floats as argument and returns
     their sum as a float."""
     total = 0
-    for i in sum_list:
+    for i in input_list:
         total = total + i
     return total
