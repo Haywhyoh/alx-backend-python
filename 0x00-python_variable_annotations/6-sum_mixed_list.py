ac74cefe9bc9fc789edde7434a6fdc0ecfae3982
Adedayo Ayomide <adefeyisayo998@gmail.com>
2023-01-05 13:29:39 +0100

    Basic annotations - sum_mixed_list

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+""" Basic annotations - sum_mixed_list """
+from typing import List, Union
+
+
+def sum_mixed_list(mxd_list: list[Union[int, float]]) -> float:
+    """ a type-annotated function sum_mixed_list which takes
+    a list mxd_lst of integers and floats and returns
+    their sum as a float."""
+    total = 0
+    for i in mxd_list:
+        total = total + i
+    return total
