6306298ee9d09e03c99dcf91c2eaf643073b374c
Adedayo Ayomide <adefeyisayo998@gmail.com>
2023-01-05 13:11:43 +0100

    a  Basic annotations - sum_list

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+""" Basic annotations - sum_list """
+
+
+def sum_list(input_list: list[float]) -> float:
+    """a type-annotated function sum_list which takes
+    a list input_list of floats as argument and returns
+    their sum as a float."""
+    total = 0
+    for i in sum_list:
+        total = total + i
+    return total
