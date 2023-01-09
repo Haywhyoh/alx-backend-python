ba0bdf053933952cdc3e82476d6c26e70cbcc822
Adedayo Ayomide <adefeyisayo998@gmail.com>
2023-01-09 19:14:20 +0100

    added an await to the func

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -10,5 +10,5 @@ async def wait_random(max_delay: int = 10) -> float:
     for a random delay between 0 and max_delay (included and float value)
     seconds and eventually returns it."""
     delay = random.uniform(0, max_delay)
-    asyncio.sleep(delay)
+    await asyncio.sleep(delay)
     return delay
