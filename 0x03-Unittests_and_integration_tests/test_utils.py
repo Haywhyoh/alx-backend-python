2269583afee8382d0438ffbf587761dffd614333
Adedayo Ayomide <adefeyisayo998@gmail.com>
2023-01-31 12:40:23 +0100

    advanced pushing

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,18 +1,19 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 """ Parameterize a unit test, Mock HTTP calls, Parameterize and patch """
 import unittest
+from unittest.mock import patch
 from parameterized import parameterized
-import utils
+from utils import access_nested_map, get_json, memoize
 
 
 class TestAccessNestedMap(unittest.TestCase):
     """ TESTCASE """
     """ to test the function for following inputs """
     @parameterized.expand([
-        ({"a": 1}, ("a",)),
-        ({"a": {"b": 2}}, ("a",)),
-        ({"a": {"b": 2}}, ("a", "b"))
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2),
     ])
     def test_access_nested_map(self, nested_map, path, answer):
         """ method to test that the method returns what it is supposed to """
-        self.assertEqual(utils.access_nested_map(nested_map, path), answer)
+        self.assertEqual(access_nested_map(nested_map, path), answer)
