538cdc258b7c859ef7a63caa21f66ee6d05e4dad
Adedayo Ayomide <adefeyisayo998@gmail.com>
2023-01-31 13:07:38 +0100

    some pycode style finishing

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -22,3 +22,7 @@ class TestAccessNestedMap(unittest.TestCase):
         ({}, "a"),
         ({"a": 1}, ("a", "b"))
     ])
+    def test_access_nested_map_exception(self, nested_map, path):
+        with self.assertRaises(KeyError) as error:
+            access_nested_map(nested_map, path)
+        self.assertEqual(error.exception.args[0], path[-1])
